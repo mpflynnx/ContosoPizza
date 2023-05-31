9ecadbd047aa82315188975dce5a89e7ec553d4c
mpflynnx <mpflynnx@gmail.com>
2023-05-31 15:45:29 +0100

    Added empty Controller named PizzaController.cs

---
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,24 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
